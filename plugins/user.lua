e4a4778873345a452e8ed33615717923473461aa
Alijan Adeli <alijan.adeli@icloud.com>
2023-05-24 22:30:56 +0200

    working config

--- plugins/user.lua
+++ plugins/user.lua
@@ -1,12 +1,24 @@
 return {
-  -- You can also add new plugins here as well:
-  -- Add plugins, the lazy syntax
-  -- "andweeb/presence.nvim",
-  -- {
-  --   "ray-x/lsp_signature.nvim",
-  --   event = "BufRead",
-  --   config = function()
-  --     require("lsp_signature").setup()
-  --   end,
-  -- },
+    -- You can also add new plugins here as well:
+    -- Add plugins, the lazy syntax
+    -- "andweeb/presence.nvim",
+    -- {
+    --   "ray-x/lsp_signature.nvim",
+    --   event = "BufRead",
+    --   config = function()
+    --     require("lsp_signature").setup()
+    --   end,
+    -- },
+    {
+        "kylechui/nvim-surround",
+        event = "User AstroFile",
+        config = function() require("nvim-surround").setup {} end,
+        keys = { "ys", "ds", "cs", { "Z", mode = "v" } },
+    },
+    {
+        "ggandor/leap.nvim",
+        event = "User AstroFile",
+        name = "leap",
+        config = function() require("leap").add_default_mappings() end,
+    },
 }
